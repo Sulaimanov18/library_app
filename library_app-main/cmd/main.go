2d184af34073bb0bc9fea7a2912a30874533df3e
Sulaimanov18 <Sulaimanov1718@gmail.com>
2025-04-17 14:33:16 -0500

    new structure

---
+++ library_app-main/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "log"
+    "github.com/gin-gonic/gin"
+    "github.com/Sulaimanov18/library_app/internal/books"
+    "github.com/Sulaimanov18/library_app/internal/common"
+)
+
+
+func main() {
+	r := gin.Default()
+
+	common.RegisterTestRoutes(r)
+	books.RegisterBookRoutes(r, books.NewBookService())
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
+
