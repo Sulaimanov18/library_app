03eaaa229d5b15b5cd4f843023dc3143f2bbf4a8
Sulaimanov18 <Sulaimanov1718@gmail.com>
2025-04-19 01:24:43 -0500

    added struct in authots model.go

--- internal/authors/model.go
+++ internal/authors/model.go
@@ -2,5 +2,16 @@ package authors
 
 
 type Authors struct{
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Bio string `json:"bio"`
+	Created string `json:"date"`
 	
+}
+
+type CreateAuthors struct {
+	Title       string `json:"title" binding:"required"`
+	Description string `json:"description"`
+	Publisher   string `json:"publisher"`
+	Count       int    `json:"count"`
 }
